db8fc638a1598ca023b91ae69a8cfecaa83a2b38
Shashank <langishashank@gmail.com>
2022-05-15 12:36:33 +0530

    [Shashank]: Finding the value of the harmonic number.

--- BasicProblems/BasicProblems/Program.cs
+++ BasicProblems/BasicProblems/Program.cs
@@ -11,7 +11,8 @@ namespace BasicProblems
         {
             Console.WriteLine("Enter the number for problem.\n");
             Console.WriteLine("1 Largest Number.");
-            Console.WriteLine("2 Even and Odd.");
+            Console.WriteLine("2 Even and Odd Number.");
+            Console.WriteLine("3 Harmonic Number.");
 
             int problem = Convert.ToInt32(Console.ReadLine());
             Program basic = new Program();
@@ -25,8 +26,12 @@ namespace BasicProblems
                     EvenOdd evenOdd = new EvenOdd();
                     evenOdd.EvenOddProgram();
                     break;
+                case 3:
+                    Harmonic harmonic = new Harmonic();
+                    harmonic.HarmonicProgram();
+                    break;
                 default:
-                    Console.WriteLine("Input is invalid,Enter the number from 1 to 2");
+                    Console.WriteLine("Input is invalid,Enter the number from 1 to 3");
                     break;
 
             }
