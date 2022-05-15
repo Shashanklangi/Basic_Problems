72c8b2fc48ebab5c666f70920de2181c77317afe
Shashank <langishashank@gmail.com>
2022-05-15 12:21:00 +0530

    [Shashank]: Finding largest among the three numbers.

---
+++ BasicProblems/BasicProblems/LargestNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProblems
+{
+    internal class LargestNumber
+    {
+        public void LargestNumberProgram()
+        {
+            Console.WriteLine("Enter the first number :");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the Second number :");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the Third number :");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                Console.WriteLine("First number is the largest number:" + num1);
+            }
+            else if (num2 > num3)
+            {
+                Console.WriteLine("Second number is the largest number:" + num2);
+            }
+            else
+            {
+                Console.WriteLine("Third number is the largest number:" + num3);
+            }
+        }
+    }
+}
---
+++ BasicProblems/BasicProblems/Program.cs
@@ -0,0 +1,17 @@
+﻿
+namespace BasicProblems
+{
+    public class Program
+    {
+        public void FunctionalPrograming()
+        {
+
+        }
+        public static void Main(string[] args)
+        {
+            LargestNumber large = new LargestNumber();
+            large.LargestNumberProgram();
+
+        }
+    }
+}
